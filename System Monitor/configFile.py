7d9cd60baf4f0491ee61dff75d053dd8e2887cc8
Defenestration123 <84874362+Defenestration123@users.noreply.github.com>
2022-05-13 17:44:34 +0200

    Add files via upload

---
+++ System Monitor/configFile.py
@@ -0,0 +1,9 @@
+#Influxdb config file
+
+
+# token = "Insert influxdb token"
+# org = "Insert influxdb org"
+# bucket = "Insert influxdb bucket"
+token = "b9NkXl9H8Qc2qkUZQjOCeBtXUsXV3RCjDuuUGUoa9fyEHiwI2h6nQvI1NKx9rkBwUphUsWool8B-osbehChd2A=="
+org = "orgName"
+bucket = "bucketName"
