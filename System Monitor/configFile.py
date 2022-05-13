8f99c9206ccfd74361b2fbd4b4899db7e6374479
Defenestration123 <84874362+Defenestration123@users.noreply.github.com>
2022-05-13 17:46:05 +0200

    Delete System Monitor directory

--- System Monitor/configFile.py
+++
@@ -1,9 +0,0 @@
-#Influxdb config file
-
-
-# token = "Insert influxdb token"
-# org = "Insert influxdb org"
-# bucket = "Insert influxdb bucket"
-token = "b9NkXl9H8Qc2qkUZQjOCeBtXUsXV3RCjDuuUGUoa9fyEHiwI2h6nQvI1NKx9rkBwUphUsWool8B-osbehChd2A=="
-org = "orgName"
-bucket = "bucketName"
